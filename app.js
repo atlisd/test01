f24280f804951bea825b62be4220611dff02586b
Atl Steinn Davíðsson <atlisd@gmail.comm>
2016-09-17 21:04:15 +0000

    jscs uppfært

--- app.js
+++ app.js
@@ -12,15 +12,14 @@ app.use(express.static('public'));
 
 app.use(express.static('src/views'));
 
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
     res.send('Hello world');
 });
 
-app.get('/books', function(req,res){
+app.get('/books', function (req, res) {
     res.send('Hello books');
 });
 
-app.listen(port, function(err) {
-        console.log('running server on port' + port);
-    }
-);
+app.listen(port, function (err) {
+    console.log('running server on port' + port);
+});
