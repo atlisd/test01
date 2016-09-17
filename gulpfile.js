4207d3a3f862b8a6854d8cff38dc6440e4cdbc68
Atl Steinn Davíðsson <atlisd@gmail.comm>
2016-09-17 22:10:55 +0000

    typos and errors in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -11,21 +11,17 @@ gulp.task('style', function() {
             verbose:true
     }))
     .pipe(jscs());
+});
    
-    gulp.task('inject', function() {
-        
+    gulp.task('inject', function() {    
         var wiredep = require('wiredep').stream;
         var options = {
             bowerJson: require('./bower.json'),
             directory: './public/lib'
-        }
+        };
         
         return gulp.src('./src/views/*.html')        
-            .pipe(wiredep(options));
+            .pipe(wiredep(options))
             .pipe(gulp.dest('./src/views/'));
-        }
+        });
         
-    })
-    
-    
-});
