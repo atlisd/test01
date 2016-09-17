56a496d93189ad6fd5321dba95b03ac24c2639b3
Atl Steinn Davíðsson <atlisd@gmail.comm>
2016-09-17 21:56:55 +0000

    meira config

--- gulpfile.js
+++ gulpfile.js
@@ -11,5 +11,21 @@ gulp.task('style', function() {
             verbose:true
     }))
     .pipe(jscs());
-             
+   
+    gulp.task('inject', function() {
+        
+        var wiredep = require('wiredep').stream;
+        var options = {
+            bowerJson: require('./bower.json'),
+            directory: './public/lib'
+        }
+        
+        return gulp.src('./src/views/*.html')        
+            .pipe(wiredep(options));
+            .pipe(gulp.dest('./src/views/'));
+        }
+        
+    })
+    
+    
 });
