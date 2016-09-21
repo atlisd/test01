39bebee73c34c0f368b5cb6d952f29bb4bf57687
Atli Steinn Davíðsson <atlid@ad.local>
2016-09-21 17:11:29 +0000

    simple book list

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
 
 // simple routing
 
@@ -12,6 +13,44 @@ app.use(express.static('public'));
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
+
+var books = [
+    {
+        title: 'War and peace',
+        genre: 'Historical Fiction',
+        author: 'Lve Nikolayevich Tolstoy',
+        read: false
+    },
+    {
+        title: 'Les Misérables',
+        genre: 'Historical Fiction',
+        author: 'Victor Hugo',
+        read: false
+    },
+    
+];
+
+bookRouter.route('/')
+    .get(function(req,res){
+        res.render('books', {
+        
+    title: 'books',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }],
+        books:books
+    })
+    });
+bookRouter.route('/single')
+    .get(function(req,res){
+        res.send('Hello single Book')
+    });
+
+app.use('/Books', bookRouter)
 
 app.get('/', function (req, res) {
     res.render('index', {
